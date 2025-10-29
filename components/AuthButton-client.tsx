72783285a1963a172af9b16350be9315aa154fc1
Gabriel Conte-Grand <gabrielcontegrand10@gmail.com>
2025-10-29 20:11:43 -0300

    fix/login-with-delay

--- components/AuthButton-client.tsx
+++ components/AuthButton-client.tsx
@@ -26,7 +26,9 @@ export const AuthButton = ({ session }: Props) => {
         redirectTo: redirectUrl,
       },
     })
-    router.push("/auth/loading")
+    setTimeout(() => {
+      router.push("/auth/loading")
+    }, 1500)
   }
 
   const handleSignOut = async () => {
